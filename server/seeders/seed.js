0d1cedd5579462dce581eee495e9299c503b2e65
Christopher Greene <chigreenwork@gmail.com>
2023-12-24 11:50:51 -0500

    fixes seed data not seeding to database by changing the saveCards field in the model to be an array of objects

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,34 +1,37 @@
+const db = require("../config/connection");
+const { User, Card } = require("../models/index");
+const cardSeeds = require("./cardSeeds.json");
+const userSeeds = require("./userSeeds.json");
+const cleanDb = require("./cleanDb");
 
-const db=require('../config/connection')
-const {User,Card}=require('../models/index')
-const cardSeeds=require('./cardSeeds.json')
-const userSeeds=require('./userSeeds.json')
-const cleanDb=require('./cleanDb')
+db.once("open", async () => {
+  try {
+    await cleanDb("Card", "cards");
+    await cleanDb("User", "users");
 
-db.once('open',async()=>{
-    try{
-         await cleanDb('Cards', 'cards');
-         await cleanDb('User', 'users');
+    const users = await User.create(userSeeds);
+    const cards = await Card.create(cardSeeds);
 
-        const users = await User.create(userSeeds);
-        const cards = await Card.create(cardSeeds);
+    if (users.length > 0 && cards.length > 0) {
+      // Select 5 random cards
+      const cardsOwned = [];
+      for (let i = 0; i < 5; i++) {
+        const randomIndex = Math.floor(Math.random() * cards.length);
+        cardsOwned.push(cards[randomIndex]._id);
+      }
 
-         if (users.length > 0 && cards.length > 0) {
-            // Push the first friend's ObjectId to the friends array of the first user
-            // Extract all friend ObjectIds
-            const cardsOwned = cards.map(card => card._id);
+      console.log("seed cards", cardsOwned);
 
-            // Push all friend ObjectIds to the first user's friends array
-            users[0].savedCards.push(...cardsOwned);
+      // Push random cardsOwned array to the savedCards of user[0]
+      users[0].savedCards.push(...cardsOwned);
 
-
-            // Save the updated user
-            await users[0].save();}
+      // Save the updated user
+      await users[0].save();
     }
-    catch(error){
-        console.error(error)
-        process.exit(1)
-    }
-    console.log('Database Seeded')
-    process.exit(0)
-})
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+  console.log("Database Seeded");
+  process.exit(0);
+});
