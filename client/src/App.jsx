8f0371ca3dfdf205e0e640c9163a5437da1f00bb
Damion Morgan <only4school5680@gmail.com>
2023-12-24 09:07:51 -0500

    changing title hoping for an error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Trading Card App</h1>
+      <h1>Trading Card Website</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
