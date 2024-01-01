b6ce92c5d3fbe7fd29089f47ebb2d9c387b4b078
Damion Morgan <only4school5680@gmail.com>
2024-01-01 14:06:05 -0500

    login and signup page added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import Home from "./pages/Home.jsx";
 import ProfilePage from "./pages/profile.jsx";
+import SignupPage from "./pages/Signup.jsx";
+import LoginPage from "./pages/Login.jsx";
 import ErrorPage from "./pages/ErrorPage.jsx";
 // import "./index.css";
 
@@ -20,6 +22,14 @@ const router = createBrowserRouter([
       {
         path:'/me',
         element:<ProfilePage></ProfilePage>
+      },
+      {
+        path:'/signup',
+        element:<SignupPage></SignupPage>
+      },
+      {
+        path:'/login',
+        element:<LoginPage></LoginPage>
       }
     ],
   },
--- client/utils/mutations.js
+++ client/utils/mutations.js
@@ -21,7 +21,7 @@ mutation Mutation($username: String!, $email: String!, $password: String!) {
 
 `
 
-export const LOGIN_IN=gql`
+export const LOGIN=gql`
 mutation Login($email: String!, $password: String!) {
   login(email: $email, password: $password) {
     user {
