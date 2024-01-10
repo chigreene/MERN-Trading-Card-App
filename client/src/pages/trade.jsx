f577cc2f953a82a8bcac5d6aeed99f160f4e4986
Damion Morgan <only4school5680@gmail.com>
2024-01-10 00:21:36 -0500

    major misunderstadning of what Auth.getProfile does

--- client/src/pages/trade.jsx
+++ client/src/pages/trade.jsx
@@ -1,5 +1,8 @@
 import './trade.css'
+import Auth from '../../utils/auth'
+
 function TradePage(){
+
   return(
     <>
     <h1>Trade Page</h1>
--- client/utils/queries.js
+++ client/utils/queries.js
@@ -1,70 +1,24 @@
 import { gql } from "@apollo/client";
 
-// export const Query_ME = gql`
-//   query Query {
-//     me {
-//       _id
-//       email
-//       username
-//       savedCards {
-//         _id
-//         card_id
-//         name
-//         rarity
-//         description
-//       }
-//     }
-//   }
-// `;
-// the old Query me
-
-// New one with Trades ( a bit longer lol)
-export const Query_ME=gql`
-query Query {
-  me {
-    _id
-    email
-    username
-    savedCards {
+export const Query_ME = gql`
+  query Query {
+    me {
       _id
-      card_id
-      name
-      rarity
-      description
-    }
-    trades {
-      _id
-      trader {
-        username
-        savedCards {
-          card_id
-          name
-          rarity
-          description
-        }
-      }
-      recipient {
-        username
-        savedCards {
-          card_id
-          name
-          rarity
-          description
-        }
-      }
-      offeredCard {
+      email
+      username
+      savedCards {
+        _id
+        card_id
         name
         rarity
+        description
       }
-      requestedCard {
-        name
-        rarity
-      }
-      status
     }
   }
-}
-`
+`;
+
+
+
 
 export const QUERY_PACK = gql`
   query Query {
