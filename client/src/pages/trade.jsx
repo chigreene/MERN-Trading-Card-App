28014afd057cf8635d9104b508b7e65766307141
Christopher Greene <chigreenwork@gmail.com>
2024-01-13 10:52:21 -0500

    fixes a merge error

--- client/src/pages/trade.jsx
+++ client/src/pages/trade.jsx
@@ -3,26 +3,60 @@ import { useMutation, useQuery } from "@apollo/client";
 import { CREATE_TRADE } from "../../utils/mutations";
 import "./trade.css";
 import { QUERY_USERS } from "../../utils/queries";
-import CreateTrade from "../components/createTrade";
 import Trades from "../components/trades";
 import Auth from "../../utils/auth";
 
 function TradePage() {
+  const profile = Auth.getProfile();
+  const username = profile?.data?.username || "";
+
+  const [formState, setFormState] = useState({
+    trader: username,
+    recipient: "",
+    offeredCard: "",
+    requestedCard: "",
+  });
+
+  const { loading, data } = useQuery(QUERY_USERS);
+  console.log("Loading state: ", loading);
+  console.log("Data object: ", data);
+
+  const [createTrade] = useMutation(CREATE_TRADE, {
+    variables: formState,
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    await createTrade();
+    window.location.reload();
+  };
+
+  // console.log("BOB123", data.users);
+
+  console.log("BOB123", formState.recipient);
+
   return (
-    <div className="container">
+    <>
       {Auth.loggedIn() ? (
         <>
           <h1>Trade Page</h1>
 
           <form onSubmit={handleFormSubmit}>
-            <label htmlFor="trader">Trader:</label>
             <input
               name="trader"
               value={formState.trader}
               type="text"
               placeholder="Trader"
             />
-            <label htmlFor="recipient">Recipient:</label>
+            {/* <label htmlFor="recipient">Recipient:</label> */}
             <select
               id="recipient"
               name="recipient"
@@ -42,7 +76,7 @@ function TradePage() {
                   ))
               )}
             </select>
-            <label htmlFor="offeredCard">Offered Card:</label>
+            {/* <label htmlFor="offeredCard">Offered Card:</label> */}
             <select
               id="offeredCard"
               name="offeredCard"
@@ -62,7 +96,7 @@ function TradePage() {
                   ))
               )}
             </select>
-            <label htmlFor="requestedCard">Requested Card:</label>
+            {/* <label htmlFor="requestedCard">Requested Card:</label> */}
             <select
               id="requestedCard"
               name="requestedCard"
@@ -85,14 +119,12 @@ function TradePage() {
 
             <button type="submit">Submit</button>
           </form>
-          <div className="tradesContainer">
-            <Trades></Trades>
-          </div>
+          <Trades></Trades>
         </>
       ) : (
         <h1>Must Be Logged In</h1>
       )}
-    </div>
+    </>
   );
 }
 
