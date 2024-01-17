51ae8820dd37aa4d4a0b6f19719855f11b967f26
Qohelet <benjy.shuter@gmail.com>
2024-01-17 10:53:30 -0500

    Adds models for planned features and fills out the inbox model.

---
+++ server/models/Inbox.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose");
+
+const inboxSchema = new Schema(
+    {
+        user:{
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+        },
+        notifications: [notificationSchema],
+        messages: [messageSchema]
+    },
+    {
+        toJSON:{
+            virtuals:true
+        },
+        id: false
+    }
+)
+
+inboxSchema.virtual('noteCount')
+.get(function(){
+    return this.notifications.length;
+});
+
+inboxSchema.virtual('fullBox')
+.get(function(){
+    function compare(a, b){
+        return a-b;
+    }
+    return this.notifications.concat(this.messages).sort(compare(a.createdAt, b.createdAt));
+});
+
+const Inbox = model('inbox', inboxSchema);
+
+module.exports = Inbox;
